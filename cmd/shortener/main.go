50a4f7355dd217ea1bf75feb6ffa20b8ca749866
AlekseyAytov <aytov88@gmail.com>
2025-01-09 15:37:08 +0300

    initial iter1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,28 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/`, mainPage)
+
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func mainPage(res http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodPost:
+		res.Header().Set("content-type", "text/plain")
+		body, _ := io.ReadAll(req.Body)
+		fmt.Println(string(body))
+	default:
+		res.WriteHeader(http.StatusBadRequest)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AlekseyAytov/go-url-shortener
+
+go 1.23.4
