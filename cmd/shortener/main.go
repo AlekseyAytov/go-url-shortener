db55e5195fc3981ede43a93d182cc18c926ad1ba
AlekseyAytov <aytov88@gmail.com>
2025-03-26 20:55:13 +0300

    some changes

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -8,25 +8,35 @@ import (
 	"github.com/AlekseyAytov/go-url-shortener/internal/config"
 	"github.com/AlekseyAytov/go-url-shortener/internal/logger"
 	"github.com/AlekseyAytov/go-url-shortener/internal/storage/db"
+	"github.com/AlekseyAytov/go-url-shortener/internal/storage/filestorage"
 	"github.com/AlekseyAytov/go-url-shortener/internal/urlobject"
 	"go.uber.org/zap"
 )
 
 func main() {
 	l := logger.Get("Info")
-	c := config.LoadOptions()
-	// fmt.Printf("%q\n%q\n", c.BaseURL, c.SrvAdress)
+	c, err := config.LoadOptions()
+	if err != nil {
+		l.Fatal("required only one storage method:",
+			zap.String("fileStoragePath", c.StoragePath),
+			zap.String("databaseDSN", c.DatabaseDSN),
+		)
+	}
+	l.Info(
+		"loaded configurtion: ",
+		zap.String("socket", c.SrvAdress),
+		zap.String("baseURL", c.BaseURL),
+		zap.String("filePath", c.StoragePath),
+		zap.String("dbDSN", c.DatabaseDSN),
+	)
 
 	mw := []func(http.Handler) http.Handler{
 		logger.RequestLogger,
 		compress.GzipMiddleware,
 	}
 
-	// storage := filestorage.NewFileStorage(c.StoragePath)
-	storage, err := db.NewDBStorage(c.DatabaseDSN)
-	if err != nil {
-		l.Fatal("error connection to db")
-	}
+	storage := getStorage(c, l)
+
 	v := urlobject.GetVault(storage)
 	api := app.NewShortenerAPI(v, c.BaseURL, mw)
 
@@ -39,4 +49,17 @@ func main() {
 		"server closed",
 		zap.Error(http.ListenAndServe(c.SrvAdress, api.Router())),
 	)
+}
+
+func getStorage(c *config.Options, l *zap.Logger) urlobject.PersistentStorage {
+	if c.StoragePath != "" {
+		return filestorage.NewFileStorage(c.StoragePath)
+	}
+
+	s, err := db.NewDBStorage(c.DatabaseDSN)
+	if err != nil {
+		l.Fatal("error connection to db",
+			zap.Error(err))
+	}
+	return s
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,11 +1,14 @@
 package config
 
 import (
+	"errors"
 	"flag"
 	"log"
 
 	"github.com/caarlos0/env/v11"
 )
+
+var ErrNotSingleStorage = errors.New("required only one storage method")
 
 // Options является структурой для парсинга настроек из переменных окружения
 type Options struct {
@@ -19,7 +22,7 @@ type Options struct {
 // - спарсить настройки из переменных окружения
 // - пустые значения взять из флагов
 // - если значений нет, назначить по умолчанию
-func LoadOptions() *Options {
+func LoadOptions() (*Options, error) {
 	cfg := Options{}
 
 	err := env.Parse(&cfg)
@@ -44,5 +47,33 @@ func LoadOptions() *Options {
 	if cfg.DatabaseDSN == "" {
 		cfg.DatabaseDSN = flagDatabaseDSN
 	}
-	return &cfg
+
+	err = cfg.checkConfig()
+	if err != nil {
+		return &cfg, err
+	}
+	cfg.fillDefaultValues()
+	return &cfg, nil
+}
+
+func (o *Options) checkConfig() error {
+	if o.StoragePath != "" && o.DatabaseDSN != "" {
+		return ErrNotSingleStorage
+	}
+	return nil
+}
+
+func (o *Options) fillDefaultValues() {
+	if o.SrvAdress == "" {
+		o.SrvAdress = ":8080"
+	}
+	if o.BaseURL == "" {
+		o.BaseURL = "http://localhost:8080"
+	}
+	if o.StoragePath == "" && o.DatabaseDSN == "" {
+		o.StoragePath = "/tmp/short-url-db.json"
+	}
+	// if o.DatabaseDSN == "" {
+	// 	o.DatabaseDSN = "postgres://supportuser:1234@localhost:5432/demo"
+	// }
 }
